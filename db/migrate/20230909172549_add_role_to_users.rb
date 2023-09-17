bbcff0f37e428b4cac65e7bd7fc7513d473d4602
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-09-17 16:05:48 +0300

    Updating migration files

--- db/migrate/20230909172549_add_role_to_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# top-level documentation comment
-class AddRoleToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :role, :string
-  end
-end
